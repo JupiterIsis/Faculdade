d448109372a426cc643dbcb3a22c3ca5a36d499a
Jupiter <jupiteryzing@gmail.com>
2022-02-08 20:43:55 -0300

    termina funcionalidade basica

--- 2022-1/Introdução a ciência da computação com python/jogoNIM.py
+++ 2022-1/Introdução a ciência da computação com python/jogoNIM.py
@@ -7,11 +7,21 @@ def partida():
         print("Pode jogar primeiro! ")
         usuario_escolhe_jogada(n, m)
 
+    placar_cmp = 0
+    placar_user = 0
+
     win = False
     while not win:
 
         n = computador_escolhe_jogada(n, m)
+        if n == 0:
+            win = True
+            print("Aww! Você perdeu!")
+
         n = usuario_escolhe_jogada(n, m)
+        if n == 0:
+            win = True
+            print("Parabéns! Você ganhou!")
         
 
 def computador_escolhe_jogada(n, m):
@@ -28,3 +38,11 @@ def computador_escolhe_jogada(n, m):
 def usuario_escolhe_jogada(n, m):
     print("Sua vez! \n")
     pecas_removidas =  int(input("Quantas peças quer retirar? "))
+    if pecas_removidas > 1 and pecas_removidas < m:
+        print("Removendo!")
+        pecas_restantes = n - pecas_removidas
+        print(f'Peças removidas por você: {pecas_removidas} \n Peças restantes: {pecas_restantes} \n')
+        return(pecas_restantes)
+    else:
+        print("Opa! Escolha um valor entre 1 e a jogada máxima, por favor. \n")
+        pecas_removidas = usuario_escolhe_jogada()
