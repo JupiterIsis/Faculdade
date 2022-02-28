07526d27e6a18427039f30519196c74f1d85bd4b
JupiterIsis <jupiteryzing@gmail.com>
2022-02-28 19:02:00 -0300

    nvm fixed lol

--- 2022-1/Criação de páginas web/Rocketseat/rocketq/src/server.js
+++ 2022-1/Criação de páginas web/Rocketseat/rocketq/src/server.js
@@ -3,10 +3,6 @@ const route = require("./route")
 
 const server = express()
 
-server.set('views', './views');
-server.set('view engine', 'ejs');
-server.engine('ejs', require('ejs').renderFile);
-
 
 server.use(route)
 server.listen(3000, () => console.log("RUNNING AT PORT 3000") )
