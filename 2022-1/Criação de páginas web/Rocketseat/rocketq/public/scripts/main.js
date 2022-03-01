ff637a881bf3a6925adae0d820f04d85fa9487de
JupiterIsis <jupiteryzing@gmail.com>
2022-02-28 22:14:58 -0300

    modal functionality

--- 2022-1/Criação de páginas web/Rocketseat/rocketq/public/scripts/main.js
+++ 2022-1/Criação de páginas web/Rocketseat/rocketq/public/scripts/main.js
@@ -1,11 +1,18 @@
-import { Modal } from "./modal"
+import Modal from "./modal.js"
 
 const modal = Modal()
 
 const rmvButtons = document.querySelectorAll(".actions a.delete")
-rmvButtons.forEach((button) => {
-    button.addEventListener("click", event => {modal.open()})
+
+// opens modal when clicking to remove a question
+rmvButtons.forEach(button => {
+    button.addEventListener("click", event => { modal.open() } )
 })
+
+
+// closes the modal once it's open
+document.getElementById("close-modal").addEventListener("click", event => {modal.close()})
+
 
 
 
--- 2022-1/Criação de páginas web/Rocketseat/rocketq/public/scripts/modal.js
+++ 2022-1/Criação de páginas web/Rocketseat/rocketq/public/scripts/modal.js
@@ -1,15 +1,16 @@
-module.exports = function Modal(){
-    modal = document.querySelector(".modal-wrapper")
-
+function Modal(){
+    const modal = document.querySelector(".modal-wrapper")
     function open(){
         modal.classList.add("active")
     }
+
     function close(){
         modal.classList.remove("active")
 
     }
     
-    return(
-        open, close
-    )
+    return {open, close}
+
 }
+
+export default Modal ;
