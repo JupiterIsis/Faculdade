f4f0ae3bd0c5d8708955aff6611e3c02bd1c12c0
JupiterIsis <jupiteryzing@gmail.com>
2022-03-01 22:55:39 -0300

    modal changes in js

--- 2022-1/Criação de páginas web/Rocketseat/rocketq/public/scripts/main.js
+++ 2022-1/Criação de páginas web/Rocketseat/rocketq/public/scripts/main.js
@@ -13,7 +13,24 @@ deleteButtons.forEach(button => {
 // closes the modal once it's open
 document.getElementById("close-modal").addEventListener("click", event => {modal.close()})
 
+
+// Make another modal for the checkmark button lmao
+const modalTitle = document.querySelector(".modal h2")
+const modalDescription = document.querySelector(".modal p")
+const modalActionButton = document.querySelector(".modal .buttons button")
+
+
+
 const checkButtons = document.querySelectorAll(".actions .check")
 checkButtons.forEach(button => {
-    button.addEventListener("click", event => {modal.open()})
+    button.addEventListener("click", event => {
+        
+        modalTitle.innerHTML = "Marcar como lida"
+        modalDescription.innerHTML = "Confirmar pergunta como lida?"
+        modalActionButton.className = "button"
+        modalActionButton.innerHTML = "Confirmar"
+    
+        modal.open()
+    
+    })
 })
