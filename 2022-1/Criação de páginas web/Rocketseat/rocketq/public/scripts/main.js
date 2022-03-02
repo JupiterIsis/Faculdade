572d76069b0cfc0cd2a7f60ed9c2b487d82e630a
JupiterIsis <jupiteryzing@gmail.com>
2022-03-02 18:06:12 -0300

    modal function handleClick

--- 2022-1/Criação de páginas web/Rocketseat/rocketq/public/scripts/main.js
+++ 2022-1/Criação de páginas web/Rocketseat/rocketq/public/scripts/main.js
@@ -4,16 +4,6 @@ const modal = Modal()
 
 const deleteButtons = document.querySelectorAll(".actions a.delete")
 
-// opens modal when clicking to remove a question
-deleteButtons.forEach(button => {
-    button.addEventListener("click", event => { modal.open() } )
-})
-
-
-// closes the modal once it's open
-document.getElementById("close-modal").addEventListener("click", event => {modal.close()})
-
-
 // Make another modal for the checkmark button lmao
 const modalTitle = document.querySelector(".modal h2")
 const modalDescription = document.querySelector(".modal p")
@@ -21,16 +11,26 @@ const modalActionButton = document.querySelector(".modal .buttons button")
 
 
 
+// opens modal when clicking to remove a question
+deleteButtons.forEach(button => {
+    button.addEventListener("click", handleClick(event))
+})
+
+
+// closes the modal once it's open
+document.getElementById("close-modal").addEventListener("click", event => {modal.close()})
+
+
 const checkButtons = document.querySelectorAll(".actions .check")
 checkButtons.forEach(button => {
-    button.addEventListener("click", event => {
-        
-        modalTitle.innerHTML = "Marcar como lida"
-        modalDescription.innerHTML = "Confirmar pergunta como lida?"
-        modalActionButton.className = "button"
-        modalActionButton.innerHTML = "Confirmar"
+    button.addEventListener("click", handleClick(event, true))
+})
+
+function handleClick(event, check = false){
+        modalActionButton.className = check ? "button" : "red"
+        modalTitle.innerHTML = check ? "Marcar como lida" : "Excluir pergunta"
+        modalDescription.innerHTML = check ? "Quer mesmo marcar pergunta como lida?" : "Tem certeza que deseja excluir essa pergunta?"
+        modalActionButton.innerHTML = check ? "Confirmar" : "Sim, excluir"
     
         modal.open()
-    
-    })
-})
+}
