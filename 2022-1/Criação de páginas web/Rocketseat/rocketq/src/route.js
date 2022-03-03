7de2b25ed8fe0eb166282ce2db6b8cf50cc74538
JupiterIsis <jupiteryzing@gmail.com>
2022-03-03 17:21:44 -0300

    add paging in router

--- 2022-1/Criação de páginas web/Rocketseat/rocketq/src/route.js
+++ 2022-1/Criação de páginas web/Rocketseat/rocketq/src/route.js
@@ -2,10 +2,10 @@ const express = require("express")
 const QuestionController = require("./controllers/QuestionController")
 const route = express.Router()
 
-route.get("/", (req, res) => res.render("index"))
+route.get("/", (req, res) => res.render("index", {page: "enter-room"}))
+route.get("/create-room", (req, res) => res.render("create-room", {page: "create-room"}))
 route.get("/room", (req, res) => res.render("room"))
-route.get("/create-room", (req, res) => res.render("create-room"))
 
-route.post('/question/:room/:question/:action', QuestionController.index)
+route.post('/room/:room/:question/:action', QuestionController.index)
 
 module.exports = route
--- 2022-1/Criação de páginas web/Rocketseat/rocketq/src/server.js
+++ 2022-1/Criação de páginas web/Rocketseat/rocketq/src/server.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const route = require("./route")
 const path = require('path')
+const exp = require("constants")
 
 const server = express()
 
@@ -8,6 +9,9 @@ server.set('view engine', 'ejs')
 server.set('views', path.join(__dirname, 'views'))
 
 server.use(express.static('public'))
+
+server.use(express.urlencoded({extended: true}))
+
 server.use(route)
 
 server.listen(3000, () => console.log("RUNNING AT PORT 3000") )
