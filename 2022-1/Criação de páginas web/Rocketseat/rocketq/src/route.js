ad23c8fafe4422f3b4c603788ebab3a759569d43
JupiterIsis <jupiteryzing@gmail.com>
2022-03-02 22:29:13 -0300

    add QuestionController

--- 2022-1/Criação de páginas web/Rocketseat/rocketq/src/route.js
+++ 2022-1/Criação de páginas web/Rocketseat/rocketq/src/route.js
@@ -1,11 +1,11 @@
 const express = require("express")
-
+const QuestionController = require("./controllers/QuestionController")
 const route = express.Router()
 
 route.get("/", (req, res) => res.render("index"))
 route.get("/room", (req, res) => res.render("room"))
 route.get("/create-room", (req, res) => res.render("create-room"))
 
-route.get("/room/:room/:question/:action", (req, res) => res.render("exemplo", {req}))
+route.post('/question/:room/:question/:action', QuestionController.index)
 
 module.exports = route
