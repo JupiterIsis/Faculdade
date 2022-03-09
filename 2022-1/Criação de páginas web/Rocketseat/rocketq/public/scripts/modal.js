9871d8c241ac1599ff3600146496e5256580aea9
JupiterIsis <jupiteryzing@gmail.com>
2022-03-09 20:31:23 -0300

    modal button fixed but still doesn't work

--- 2022-1/Criação de páginas web/Rocketseat/rocketq/public/scripts/modal.js
+++ 2022-1/Criação de páginas web/Rocketseat/rocketq/public/scripts/modal.js
@@ -1,16 +1,21 @@
-function Modal(){
-    const modal = document.querySelector(".modal-wrapper")
+export default function Modal(){
+
+    const modalWrapper = document.querySelector('.modal-wrapper')
+    const cancelButton = document.querySelector('.button.cancel')
+
+    cancelButton.addEventListener("click", close)
+
     function open(){
-        modal.classList.add("active")
+        //funcionalidade de atribuir a classe active para a modal
+        modalWrapper.classList.add("active")
     }
-
     function close(){
-        modal.classList.remove("active")
-
+        //funcionalidade de remover a classe active da modal
+        modalWrapper.classList.remove("active")
     }
-    
-    return {open, close}
 
+    return{
+        open,
+        close
+    }
 }
-
-export default Modal ;
--- 2022-1/Criação de páginas web/Rocketseat/rocketq/src/controllers/QuestionController.js
+++ 2022-1/Criação de páginas web/Rocketseat/rocketq/src/controllers/QuestionController.js
@@ -5,31 +5,25 @@ module.exports = {
     
     async index(req, res){
         const db = await Database()
+        
         const roomId = req.params.room
         const questionId = req.params.question
         const action = req.params.action
         const password = req.body.password
-
-        /* Verificar se a senha está correta */
-        const verifyRoom = await db.get(`SELECT * FROM rooms WHERE id = ${roomId}`)
-        if(verifyRoom.pass == password){
-            if(action == "delete"){
-
-                await db.run(`DELETE FROM questions WHERE id = ${questionId}`)
-
-            }else if(action == "check"){
-
-                await db.run(`UPDATE questions SET read = 1 WHERE id = ${questionId}`)
-
+        
+        // Verify password  
+        const verifyRoom = await db.all(`SELECT * FROM rooms WHERE id = ${roomId}`)
+        if (verifyRoom.password == password) {
+            if(action == "check"){
+                await db.run(`UPDATE questions SET read = 1 WHERE id = ${questionId}`)                
+            } else if(action == "delete") {
+                await db.run(`DELETE FROM questions where id = ${questionId}`)
             }
             res.redirect(`/room/${roomId}`)
-        } else{
-            res.render('passincorrect', {roomId: roomId})
         }
 
 
     },
-
 
     async create(req,res){
         const db = await Database()
