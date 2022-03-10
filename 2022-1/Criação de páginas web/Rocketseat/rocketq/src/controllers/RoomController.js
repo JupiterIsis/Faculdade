7538b9caf11f29c07529417f33518c7de1317831
JupiterIsis <jupiteryzing@gmail.com>
2022-03-10 20:05:49 -0300

    finish no questions room. Functional?

--- 2022-1/Criação de páginas web/Rocketseat/rocketq/src/controllers/RoomController.js
+++ 2022-1/Criação de páginas web/Rocketseat/rocketq/src/controllers/RoomController.js
@@ -42,8 +42,14 @@ module.exports = {
         const roomId = req.params.room
         const questions = await db.all(`SELECT * FROM questions WHERE roomId = ${roomId} and read = 0`)
         const questionsRead = await db.all(`SELECT * FROM questions WHERE roomId = ${roomId} and read = 1`)
-        
-        res.render("room", {roomId: roomId, questions: questions, questionsRead: questionsRead})
+        let isQuestions = true
+
+        if (questions.length === 0 && questionsRead.length === 0) {
+            isQuestions = false
+        }
+
+
+        res.render("room", {roomId: roomId, questions: questions, questionsRead: questionsRead, isQuestion: isQuestions})
     },
 
     enter(req, res){
