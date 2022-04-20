0dbb5672ddce879ad80b99f8d64d3e661bf94648
JupiterIsis <jupiteryzing@gmail.com>
2022-04-20 15:00:55 -0300

    memory popup

--- 2022-1/Criação de páginas web/AD1 2022-1/HTML5/scripts/memorias.js
+++ 2022-1/Criação de páginas web/AD1 2022-1/HTML5/scripts/memorias.js
@@ -1,11 +1,10 @@
 
 function abrirJanela(index){
     let memorias = document.querySelectorAll(".link_memoria");
-    console.log(memorias)
     window.open(
         url="memorias/showMemory.html?index=" + index,
         target= memorias[index].innerHTML,
-        features= 'height=500,width=400,left=100,top=100'
+        features= 'height=450,width=225,left=100,top=100'
         );
     return false; 
 }
--- 2022-1/Criação de páginas web/AD1 2022-1/HTML5/scripts/mostraMemoria.js
+++ 2022-1/Criação de páginas web/AD1 2022-1/HTML5/scripts/mostraMemoria.js
@@ -1,6 +1,7 @@
 const lista_memorias = [
     {
         tipo:"DDR3",
+        src: "/assets/Mem_Kingston_4gb_200.jpg",
         fabricante:"Kingston",
         capacidade: "4Gb",
         preco: "150",
@@ -8,20 +9,46 @@ const lista_memorias = [
     },
     {
         tipo:"DDR3",
+        src: "/assets/Mem_Samsung_8g_200.jpg",
         fabricante: "Samsung",
         capacidade: "8Gb",
         preco:"300"
     },
     {
         tipo: "DDR4",
+        src: "/assets/Mem_Fury_8gb_200.jpg",
         fabricante: "Kingston Fury",
         capacidade: "8Gb",
         preco:"250"
     },
     {
         tipo: "DDR4",
+        src: "/assets/Mem_Corsair_8gb_200.jpg",
         fabricante: "Corsair Vengeance",
         capacidade: "8Gb",
         preco:"270"
     }
 ]
+
+
+// preenche os campos HTML com as informações adequadas
+function populateHTML(){
+    // pega os query params, transforma em URLSearchParams obj, pega o valor da chave "index" e transforma em int
+    var indexMemoria = parseInt(new URLSearchParams(location.search).get("index"));
+
+    var tipo = document.getElementById("tipo");
+    var imgMemoria = document.getElementById("imgMemoria");
+    var fabricante = document.getElementById("fabricante");
+    var capacidade = document.getElementById("capacidade");
+    var preco = document.getElementById("preco");
+    
+    console.log(tipo)
+    
+    tipo.innerHTML = "Tipo " + lista_memorias[indexMemoria].tipo
+    imgMemoria.src = lista_memorias[indexMemoria].src
+    fabricante.innerText = "Fabricante: " + lista_memorias[indexMemoria].fabricante
+    capacidade.innerText = "Capacidade: " + lista_memorias[indexMemoria].capacidade
+    preco.innerText = "Preço: R$" + lista_memorias[indexMemoria].preco
+}
+
+
