b579ceab48aa3273911a511b2855e802b59a3d98
JupiterIsis <jupiteryzing@gmail.com>
2022-04-25 17:39:15 -0300

    AD2 CPW

--- 2022-1/Criação de páginas web/AD1 2022-1/HTML5/scripts/compras.js
+++ 2022-1/Criação de páginas web/AD1 2022-1/HTML5/scripts/compras.js
@@ -1,17 +1,128 @@
-import {lista_memorias, lista_hds} from "./produtos";   
+const lista_memorias = [
+    {
+        tipo:"DDR3",
+        src: "/assets/Mem_Kingston_4gb_200.jpg",
+        fabricante:"Kingston",
+        capacidade: "4Gb",
+        preco: "150,00",
+        
+    },
+    {
+        tipo:"DDR3",
+        src: "/assets/Mem_Samsung_8g_200.jpg",
+        fabricante: "Samsung",
+        capacidade: "8Gb",
+        preco:"300,00"
+    },
+    {
+        tipo: "DDR4",
+        src: "/assets/Mem_Fury_8gb_200.jpg",
+        fabricante: "Kingston Fury",
+        capacidade: "8Gb",
+        preco:"250,00"
+    },
+    {
+        tipo: "DDR4",
+        src: "/assets/Mem_Corsair_8gb_200.jpg",
+        fabricante: "Corsair Vengeance",
+        capacidade: "8Gb",
+        preco:"270,00"
+    }
+]
+const lista_hds = [
+    {   
+        fabricante: "Western Digital",
+        src: "/assets/HD_WD_500gb_70.jpg",
+        interface: "HD SATA III",
+        capacidade: "500 Gb",
+        preco: "170,00"
+    },
+    {   
+        fabricante: "Seagate",
+        src: "/assets/HD_Seagate_1Tb_70.jpg",
+        interface: "HD SATA III",
+        capacidade: "1 Tb",
+        preco: "350,00"
+    },
+    {   
+        fabricante: "Samdisk",
+        src: "/assets/SSD_Samdisk_480Gb_70.jpg",
+        interface: "SSD SATA III",
+        capacidade: "480 Gb",
+        preco: "380,00"
+    },
+    {   
+        fabricante: "Kingston",
+        src: "/assets/SSD_Kingston_960Gb_70.jpg",
+        interface: "SSD SATA III",
+        capacidade: "960 Gb",
+        preco: "700,00"
+    },
+    {   
+        fabricante: "Kingston",
+        src: "/assets/SSD_Kingston_M2_5000Gb_70.jpg",
+        interface: "SSD M2",
+        capacidade: "500 Gb",
+        preco: "410,00"
+    },
+    {   
+        fabricante: "Western Digital",
+        src: "/assets/SSD_WD_M2_240Gb_70.jpg",
+        interface: "SSD M2",
+        capacidade: "240 Gb",
+        preco: "270,00"
+    },
+]
 
+let preco, texto; 
 function populateListeners(){
     const options = document.querySelectorAll("option")
     options.forEach(element => {
         element.addEventListener("click", (e) => {
-            sumPrice(element)
+            setProductFields(element)
         })
         
+        
     });
+    const buttonApply = document.querySelector("[name=applyFields]")
+    buttonApply.addEventListener("click", (e) => {
+        e.preventDefault()
+        applyProductFields(preco, texto)
+
+    })
 }
 
-function sumPrice(element){
-    const campoValor = document.querySelector("[name=valor]")
-    console.log(element.data.type)
+
+function setProductFields(element){
+    if (element.value == "-----"){
+        preco = undefined
+        texto = undefined
+    }
+    switch(element.dataset.type){
+        case "memoria":
+            preco = parseInt(lista_memorias[element.dataset.index].preco)
+            texto = "Memória " + lista_memorias[element.dataset.index].fabricante + " " + lista_memorias[element.dataset.index].capacidade + "\n"
+            break;
+        case "hd":
+            preco = parseInt(lista_hds[element.dataset.index].preco)
+            texto = lista_hds[element.dataset.index].interface + " " + lista_hds[element.dataset.index].fabricante + " " + lista_hds[element.dataset.index].capacidade + "\n"
+            break;
+        }
+    campoValor.value = parseInt(campoValor.value) + preco
+    campoTexto.value = campoTexto.value + texto
     // switch(element.data.type
+}
+
+function applyProductFields(preco, texto){
+    const campoValor = document.querySelector("[name=valor]")
+    const campoTexto = document.querySelector("[name=lista-produtos]")
+
+    if (texto){
+        campoValor.value = String(parseInt(campoValor.value) + preco) + ",00"
+        campoTexto.value = campoTexto.value + texto
+        
+    } else {
+        alert("Nenhum produto selecionado!")
+    }
+
 }
--- 2022-1/Criação de páginas web/AD1 2022-1/HTML5/scripts/hds.js
+++ 2022-1/Criação de páginas web/AD1 2022-1/HTML5/scripts/hds.js
@@ -1,4 +1,48 @@
-import {lista_hds} from "./produtos"
+lista_hds = [
+    {   
+        fabricante: "Western Digital",
+        src: "/assets/HD_WD_500gb_70.jpg",
+        interface: "HD SATA III",
+        capacidade: "500 Gb",
+        preco: "170,00"
+    },
+    {   
+        fabricante: "Seagate",
+        src: "/assets/HD_Seagate_1Tb_70.jpg",
+        interface: "HD SATA III",
+        capacidade: "1 Tb",
+        preco: "350,00"
+    },
+    {   
+        fabricante: "Samdisk",
+        src: "/assets/SSD_Samdisk_480Gb_70.jpg",
+        interface: "SSD SATA III",
+        capacidade: "480 Gb",
+        preco: "380,00"
+    },
+    {   
+        fabricante: "Kingston",
+        src: "/assets/SSD_Kingston_960Gb_70.jpg",
+        interface: "SSD SATA III",
+        capacidade: "960 Gb",
+        preco: "700,00"
+    },
+    {   
+        fabricante: "Kingston",
+        src: "/assets/SSD_Kingston_M2_5000Gb_70.jpg",
+        interface: "SSD M2",
+        capacidade: "500 Gb",
+        preco: "410,00"
+    },
+    {   
+        fabricante: "Western Digital",
+        src: "/assets/SSD_WD_M2_240Gb_70.jpg",
+        interface: "SSD M2",
+        capacidade: "240 Gb",
+        preco: "270,00"
+    },
+
+]
 
 function addEventListeners(){
     const hds = document.querySelectorAll(".hd")
